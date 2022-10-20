f7267caa2a9adf858bff13f1a0d6dc3930f12dc5
vcore <vcorelaile@gmail.com>
2022-10-20 12:25:29 +0800

    update

---
+++ chap2/target/debug/chap2.d
@@ -0,0 +1,1 @@
+/home/vcore/ss/chap2/target/debug/chap2: /home/vcore/ss/chap2/src/main.rs
---
+++ chap2/target/debug/deps/chap2-5004d392fd7c98e5.d
@@ -0,0 +1,5 @@
+/home/vcore/ss/chap2/target/debug/deps/chap2-5004d392fd7c98e5: src/main.rs
+
+/home/vcore/ss/chap2/target/debug/deps/chap2-5004d392fd7c98e5.d: src/main.rs
+
+src/main.rs:
